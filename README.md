04eaa6b6b80d1141c5093fcd92d4c97085352c88
supkc <supkc94@gmail.com>
2021-05-20 00:02:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vv7yfe-afwczk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vv7yfe-afwczk)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,24 @@
+// import { Injectable } from '@angular/core';
+// import { products } from './products';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class CartService {
+//   items: any;
+
+//   constructor() {}
+
+//   addToCart(product: any) {
+//     this.items.push(product);
+//   }
+
+//   getItems() {
+//     return this.items;
+//   }
+
+//   clearCart() {
+//     this.items = [];
+//     return this.items;
+//   }
+// }
